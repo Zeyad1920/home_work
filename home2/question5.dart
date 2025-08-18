746b7aa930bd6d96aa753a30cb48db9e49dd9cd6
Zyad <Zeyad1920@gmail.com>
2025-08-18 20:35:27 +0300

    home2Done

---
+++ home2/question5.dart
@@ -0,0 +1,19 @@
+void main() {
+  int a = 5;
+  int b = 10;
+  bool isEqual = a == b;
+  bool isNotEqual = a != b;
+  bool isaGreater = a > b;
+  bool isaLess = a < b;
+  bool isaGreaterOrEqual = a >= b;
+  bool isaLessOrEqual = a <= b;
+  print("Is Equal: $isEqual");
+  print("Is Not Equal: $isNotEqual");
+  print("Is a Greater: $isaGreater");
+  print("Is a Less: $isaLess");
+  print("Is a Greater or Equal: $isaGreaterOrEqual");
+  print("Is a Less or Equal: $isaLessOrEqual");
+  int sum = a + b;
+  bool isSumEqual20 = sum == 20;
+  print("Is Sum Equal to 20: $isSumEqual20");
+}
