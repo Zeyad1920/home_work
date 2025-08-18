14f63655a3c3ec24f5aa8a7bfff07b44c561589f
Zyad <Zeyad1920@gmail.com>
2025-08-18 18:49:46 +0300

    home2done

---
+++ home2/question2.dart
@@ -0,0 +1,11 @@
+void main() {
+  String country = "Egypt";
+  int year = 2025;
+  double weight = 75.5;
+  bool likesCoding = true;
+  print(
+    "My country is $country and I will be in $year, my weight is $weight kg and do I like coding? $likesCoding",
+  );
+  weight = 80.0;
+  print("My updated weight is $weight kg.");
+}
