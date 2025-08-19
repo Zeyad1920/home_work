a537251df470c7a52aade87d8bd24da2670ea7cd
Zyad <Zeyad1920@gmail.com>
2025-08-19 15:37:45 +0300

    home2FinshDone

---
+++ home2/question10.dart
@@ -0,0 +1,12 @@
+void main() {
+  dynamic variable = 10; // as int
+  print(variable);
+  variable = "welcome guys"; // now as String
+  print(variable);
+  var greeting = 'Hi'; // as String
+  greeting = 'Hello'; // as String
+  print(greeting);
+  num pi = 3.14159;
+  print(pi.toInt());
+  print(pi.toStringAsFixed(3));
+}
