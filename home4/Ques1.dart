ea0744baad9e26ba72538fcdd6314c19cee8736c
Zyad <Zeyad1920@gmail.com>
2025-08-26 01:45:26 +0300

    home4Done

--- home4/Ques1.dart
+++ home4/Ques1.dart
@@ -3,3 +3,6 @@ void main() {
   Set<int> uniqueNumbers = numbers.toSet();
   print(uniqueNumbers);
 }
+/* Q1
+ Create a program that removes duplicate numbers from the list [5, 3, 5, 7, 3, 9] and prints how
+ many unique numbers remain*/
