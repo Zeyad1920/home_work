eb14b77e4e07d584dea073337cf04a65a87ebe1b
Zyad <Zeyad1920@gmail.com>
2025-08-26 01:35:07 +0300

    home4Done

---
+++ home4/Ques12.dart
@@ -0,0 +1,12 @@
+void main() {
+  String? username;
+
+  if (username == null || username.isEmpty) {
+    print("guest");
+  } else {
+    print("username");
+  }
+}
+/*Q12
+ Create a program with a username that is empty. If the username is empty, print (guest). Otherwise,
+ print the username */ 
