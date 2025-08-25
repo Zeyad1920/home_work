e417b6d1b72f576e010cd3ce3985ddadf04a0158
Zyad <Zeyad1920@gmail.com>
2025-08-26 00:54:20 +0300

    home4Done

---
+++ home4/Ques6.dart
@@ -0,0 +1,9 @@
+void main() {
+  int Sum = 1;
+  for (int i = 1; i <= 6; i++) {
+    Sum = Sum * i;
+  }
+  print(Sum);
+}
+/*Q6
+ Create a program that calculates the factorial of 6 and prints the result */
