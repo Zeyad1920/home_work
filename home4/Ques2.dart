ee87685c8545bdca126863ee2d96abf17f8eacea
Zyad <Zeyad1920@gmail.com>
2025-08-26 00:12:40 +0300

    home4Done

---
+++ home4/Ques2.dart
@@ -0,0 +1,9 @@
+void main() {
+  Map<String, String> countryCode = {
+    'EG': 'Egypt',
+    'SA': 'Saudi Arabia',
+    'AE': 'UAE',
+  };
+  countryCode['QA'] = 'Qatar';
+  print(countryCode['EG']);
+}
