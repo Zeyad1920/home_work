8b74a2411f16b7da1dce067d226046417f71f196
Zyad <Zeyad1920@gmail.com>
2025-08-31 15:40:21 +0300

    home5Done

---
+++ home5/Ques2.dart
@@ -0,0 +1,13 @@
+import 'dart:collection';
+
+void main() {
+  List<double> numbers = [5, 10, 15, 20, 25];
+  double sum = 0;
+  for (int i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  sum = sum / numbers.length;
+  print("The average is: $sum");
+}
+/* Q2
+ Create a program with the list [5, 10, 15, 20, 25]. Print the average of the numbers */
