d7f7b8a6ec626ff23a772a58f81a29289034d101
Zyad <Zeyad1920@gmail.com>
2025-08-31 15:45:04 +0300

    home5Done

---
+++ home5/Ques3.dart
@@ -0,0 +1,8 @@
+void main() {
+  Set<int> numbers = {3, 6, 9, 3, 12, 15};
+  bool isNumbersContain10 = numbers.contains(10);
+  print("Does the set contain 10? $isNumbersContain10");
+}
+/* Q3
+ Create a program with a set of numbers [3, 6, 9, 3, 12, 15]. Print whether the set contains the
+ number 10 */
