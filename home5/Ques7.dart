4f2961a3c25f568d4ad20d02acae019724f14fa7
Zyad <Zeyad1920@gmail.com>
2025-09-01 14:21:40 +0300

    home5Done

---
+++ home5/Ques7.dart
@@ -0,0 +1,8 @@
+void main() {
+  String noun = "Welcome to Dart";
+  print(noun.toUpperCase());
+  print(noun.length);
+}
+/* Q7
+ Create a program with the string 'Welcome to Dart'. Print the string in uppercase and also print its
+ length*/
