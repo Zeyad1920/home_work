a732050289d9ea0c74d1456bfd7eedae5b3221bd
Zyad <Zeyad1920@gmail.com>
2025-09-01 14:26:07 +0300

    home5Done

---
+++ home5/Ques8.dart
@@ -0,0 +1,14 @@
+void main() {
+  int? bonus;
+  bonus = 60;
+  if (bonus > 50) {
+    print("Big bonus");
+  } else if (bonus <= 50) {
+    print("Small bonus");
+  } else {
+    print("No bonus");
+  }
+}
+/*Q8
+ Create a program with a nullable integer called bonus. If it has a value greater than 50, print 'Big
+ bonus'. If it has a value but less than or equal to 50, print 'Small bonus'. If it is null, print 'No bonus'. */
