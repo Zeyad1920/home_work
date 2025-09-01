33077ebf2ac6deb51e85f35a3da0b9864ab297b8
Zyad <Zeyad1920@gmail.com>
2025-09-01 14:16:32 +0300

    home5Done

---
+++ home5/Ques5.dart
@@ -0,0 +1,12 @@
+void main() {
+  bool isMember = false;
+  int point = 120;
+  if (isMember == true && point > 100) {
+    print("Eligible for reward.");
+  } else {
+    print("Not eligible .");
+  }
+}
+/* Q5
+ Create a program with a boolean isMember = false and an integer points = 120. If isMember is true
+ and points >= 100, print 'Eligible for reward'. Otherwise, print 'Not eligible' */
